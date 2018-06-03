06c92a7f9a2d219e4f0634e5c8477bb5da958bef
mawpa <kuziemski.a@gmail.com>
2018-06-03 10:53:00 +0200

    included MatButtonModule

--- src/app/dialogue/line-container/line-container.component.spec.ts
+++ src/app/dialogue/line-container/line-container.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { By } from '@angular/platform-browser';
 
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
 import { DialogueLine } from '@dialogue/dialogue-line';
@@ -17,6 +18,7 @@ describe('LineContainerComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
+        MatButtonModule,
         MatIconModule
       ],
       declarations: [
