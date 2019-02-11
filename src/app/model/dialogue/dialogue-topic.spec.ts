c0bd0ec7aa9ae2337caa1e0f5e847e7ff7fa4fef
kuziemski.a@gmail.com <kuziemski.a@gmail.com>
2019-02-11 21:03:43 +0100

    fixed topic tests

--- src/app/model/dialogue/dialogue-topic.spec.ts
+++ src/app/model/dialogue/dialogue-topic.spec.ts
@@ -1,7 +1,11 @@
 import { DialogueTopic } from './dialogue-topic';
 
 describe('DialogueTopic', () => {
-  const topic = new DialogueTopic('Hello');
+  let topic: DialogueTopic;
+
+  beforeEach(() => {
+    topic = new DialogueTopic('Hello');
+  });
 
   it('should be empty right after creation', () => expect(topic.empty).toBe(true));
 
@@ -12,12 +16,19 @@ describe('DialogueTopic', () => {
   });
 
   it('should swap lines successfully', () => {
+    topic.addLine('Are you the Tester?');
+    topic.addLine('I thought you would come sooner or later');
+
     topic.swapLines(0, 1);
+
     expect(topic.lines[0].line).toBe('I thought you would come sooner or later');
     expect(topic.lines[1].line).toBe('Are you the Tester?');
   });
 
   it('should remove lines successfully', () => {
+    topic.addLine('Are you the Tester?');
+    topic.addLine('I thought you would come sooner or later');
+
     topic.removeLine(1);
     expect(topic.lines.length).toBe(1);
   });
@@ -29,13 +40,21 @@ describe('DialogueTopic', () => {
   });
 
   it(`should have 4 lines (including children's lines)`, () => {
-    topic.addLine('Are you the Tester?'); // we removed it earlier, add it back
+    topic.addLine('Are you the Tester?');
+    topic.addLine('I thought you would come sooner or later');
+
+    topic.addTopic('Yes, I am.');
+    topic.addTopic('Are you?');
+
     topic.topics[0].addLine('Whoa! I knew it!');
     topic.topics[1].addLine(`Do you know something I don't?`);
     expect(topic.length).toBe(4);
   });
 
-  it(`should have 4 topics (including children's topics)`, () => {
+  it(`should have 5 topics (including children's topics)`, () => {
+    topic.addTopic('Yes, I am.');
+    topic.addTopic('Are you?');
+
     topic.topics[0].addTopic('I need you to keep it a secret, though');
     topic.topics[0].topics[0].addTopic('Glad we could make it clear');
     topic.topics[1].addTopic('Maybe.');
