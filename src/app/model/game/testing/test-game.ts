2e4753ccda53887588e7ce771ca248df834569cb
kuziemski.a@gmail.com <kuziemski.a@gmail.com>
2019-02-24 15:52:55 +0100

    changed test data to show initial values in the item list

--- src/app/model/game/testing/test-game.ts
+++ src/app/model/game/testing/test-game.ts
@@ -7,16 +7,44 @@ export function createTestGame(): Game {
 
   testItemTypes.forEach(type => testGame.createItemType(type));
 
-  testGame.createItem('TestItemArchontArmor', 'Archont Armor').type = testItemTypes[0];
-  testGame.createItem('TestItemDirtyMagazine', 'Magazine').type = testItemTypes[1];
-  testGame.createItem('TestItemPotion', 'Potion').type = testItemTypes[2];
-  testGame.createItem('TestItemSuperPotion', 'Super Potion').type = testItemTypes[2];
-  testGame.createItem('TestItemUmbra', 'Umbra').type = testItemTypes[3];
-  testGame.createItem('TestItemDPUH', 'Double Penetrating Unkempt Harold').type = testItemTypes[3];
+  let archont = testGame.createItem('TestItemArchontArmor', 'Archont Armor');
+  archont.type = testItemTypes[0];
+  archont.isWearable = true;
+  archont.value = 1337;
+  archont.weight = 100;
+
+  let magazine = testGame.createItem('TestItemDirtyMagazine', 'Magazine');
+  magazine.type = testItemTypes[1];
+  magazine.isReadable = true;
+  magazine.value = 25;
+  magazine.weight = 1;
+
+  let potion = testGame.createItem('TestItemPotion', 'Potion');
+  potion.type = testItemTypes[2];
+  potion.value = 50;
+  potion.weight = 0.5;
+
+  let superPotion = testGame.createItem('TestItemSuperPotion', 'Super Potion');
+  superPotion.type = testItemTypes[2];
+  superPotion.value = 100;
+  superPotion.weight = 0.5;
+
+  let umbra = testGame.createItem('TestItemUmbra', 'Umbra');
+  umbra.type = testItemTypes[3];
+  umbra.value = 2000;
+  umbra.weight = 32;
+
+  let dpuh = testGame.createItem('TestItemDPUH', 'Double Penetrating Unkempt Harold');
+  dpuh.type = testItemTypes[3];
+  dpuh.value = 617;
+  dpuh.weight = 10;
 
   testGame.createNPC('TestNPCGossip', 'Wirt');
   testGame.createNPC('TestNPCEnemy', 'Gary');
-  testGame.createNPC('TestNPCMerchant', 'Creeper');
+
+  let creeper = testGame.createNPC('TestNPCMerchant', 'Creeper');
+  creeper.isMerchant = true;
+
   testGame.createNPC('TestNPCTalker', 'Lucien Lachance');
   testGame.createNPC('TestNPCQuestGiver', 'Craptrap');
 
