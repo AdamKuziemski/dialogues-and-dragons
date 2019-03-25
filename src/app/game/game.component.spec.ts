747bcc860e81f6c0d26e72619b429334ea16d614
kuziemski.a@gmail.com <kuziemski.a@gmail.com>
2019-03-25 23:22:46 +0100

    created tests for game component

--- src/app/game/game.component.spec.ts
+++ src/app/game/game.component.spec.ts
@@ -1,11 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { By } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
-import { GameComponent } from './game.component';
-import { MatButtonModule, MatIconModule, MatTabsModule } from '@angular/material';
+import { MatButtonModule, MatDialogModule, MatIconModule, MatTabsModule } from '@angular/material';
+import { MatDialog } from '@angular/material';
 
-xdescribe('GameComponent', () => {
+import { GameComponent } from './game.component';
+import { GameService } from '@game/game.service';
+import { ResponsiveService } from '@responsive-service';
+
+import { click } from '@testing/click.function';
+import { MockHammerJs } from '@testing/hammer-js-mock';
+
+describe('GameComponent', () => {
   let component: GameComponent;
   let fixture: ComponentFixture<GameComponent>;
 
@@ -14,18 +22,36 @@ xdescribe('GameComponent', () => {
       declarations: [GameComponent],
       imports: [
         MatButtonModule,
+        MatDialogModule,
         MatIconModule,
         MatTabsModule,
         NoopAnimationsModule
       ],
-      providers: [],
-      schemas: [NO_ERRORS_SCHEMA] // for now, will create some tests later
+      providers: [
+        GameService,
+        MatDialog,
+        MockHammerJs,
+        ResponsiveService
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents().then(() => {
       fixture = TestBed.createComponent(GameComponent);
       component = fixture.componentInstance;
       fixture.detectChanges();
+
+      spyOn(component, 'addEntity');
     });
   }));
 
   it('should create', () => expect(component).toBeTruthy());
+
+  it('should have 4 tabs',
+    () => expect(fixture.debugElement.queryAll(By.css('.mat-tab-label')).length).toBe(4)
+  );
+
+  it('should open a dialog when the fab is clicked', () => {
+    click(fixture.debugElement.query(By.css('button')));
+    expect(component.addEntity).toHaveBeenCalled();
+  });
+
 });
