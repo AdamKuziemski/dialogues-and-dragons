5db8def0062d30dbf312cf1ff07bdaf14ccdbfd8
kuziemski.a@gmail.com <kuziemski.a@gmail.com>
2019-04-16 07:33:18 +0200

    hosting on lan for mobile testing

--- src/app/condition/condition.module.ts
+++ src/app/condition/condition.module.ts
@@ -1,34 +1,31 @@
 import { NgModule } from '@angular/core';
 
-import { ConditionDependent } from '../model/condition/condition-dependent';
+// import { ConditionDependent } from '../model/condition/condition-dependent';
 
-import { DispositionTowardsPlayer } from '../model/condition/disposition-towards-player.condition';
-import { GetItemCount } from '../model/condition/get-item-count.condition';
-import { HasItem } from '../model/condition/has-item.condition';
-import { QuestStage } from '../model/condition/quest-stage.condition';
+// import { DispositionTowardsPlayer } from '../model/condition/disposition-towards-player.condition';
+// import { GetItemCount } from '../model/condition/get-item-count.condition';
+// import { HasItem } from '../model/condition/has-item.condition';
+// import { QuestStage } from '../model/condition/quest-stage.condition';
 
 @NgModule({
   declarations: [
-    ConditionDependent,
-    DispositionTowardsPlayer,
-    GetItemCount,
-    HasItem,
-    QuestStage
+    // DispositionTowardsPlayer,
+    // GetItemCount,
+    // HasItem,
+    // QuestStage
   ],
   imports: [
-    ConditionDependent,
-    DispositionTowardsPlayer,
-    GetItemCount,
-    HasItem,
-    QuestStage
+    // DispositionTowardsPlayer,
+    // GetItemCount,
+    // HasItem,
+    // QuestStage
   ],
   providers: [ ],
   exports: [
-    ConditionDependent,
-    DispositionTowardsPlayer,
-    GetItemCount,
-    HasItem,
-    QuestStage
+    // DispositionTowardsPlayer,
+    // GetItemCount,
+    // HasItem,
+    // QuestStage
   ]
 })
 export class ConditionModule { }
