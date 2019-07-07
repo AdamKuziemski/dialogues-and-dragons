d761b14ecc97d2e2176fd11d713382b4b7b24fb7
kuziemski.a@gmail.com <kuziemski.a@gmail.com>
2019-07-07 16:28:25 +0200

    changed the name to be more appropriate

--- src/app/model/quest/testing/test-quest.ts
+++ src/app/model/quest/testing/test-quest.ts
@@ -4,7 +4,7 @@ export { Quest } from '../quest'; // convenience
 export { QuestStage } from '../quest-stage'; // convenience
 
 export function createTestQuest(): Quest {
-  const testQuest = new Quest('Plumber brothers: Green and Red');
+  const testQuest = new Quest('We Need To Go Deeper');
 
   testQuest.addStage('I heard a rumor that somebody is creating a game editor. I should look for them and see if I can help them.');
   testQuest.addStage('I just heard they live in Lodz. I should head there.');
