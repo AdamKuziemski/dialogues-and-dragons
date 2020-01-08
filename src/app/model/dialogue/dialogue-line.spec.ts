aae720be76afe3829be2de815bb6797791cfc8db
kuziemski.a@gmail.com <kuziemski.a@gmail.com>
2020-01-08 22:45:13 +0100

    created LineContainer

--- src/app/model/dialogue/dialogue-line.spec.ts
+++ src/app/model/dialogue/dialogue-line.spec.ts
@@ -1,3 +1,75 @@
+import { LineContainer } from './dialogue-line';
+
 xdescribe('DialogueLine', () => {
   // empty for now
+});
+
+describe('LineContainer', () => {
+  let container: LineContainer;
+
+  const fill = () => container.add(
+    'Chicken chaser! Do you chase chickens?',
+    'I used to be an adventurer, like you. Then I took an arrow in the knee',
+    `You'll never take me alive, you robotic sumbitch!`
+  );
+
+  beforeEach(() => container = new LineContainer());
+
+  it('should add single lines', () => {
+    expect(container.empty).toBe(true);
+
+    container.add('Hurro');
+    expect(container.length).toBe(1);
+
+    expect(container.empty).toBe(false);
+  });
+
+  it('should add multiple lines', () => {
+    container.add(
+      'Chicken chaser! Do you chase chickens?',
+      'I used to be an adventurer, like you. Then I took an arrow in the knee',
+      `You'll never take me alive, you robotic sumbitch!`
+    );
+
+    expect(container.length).toBe(3);
+  });
+
+  it('should remove lines', () => {
+    fill();
+    container.remove(1);
+    expect(container.length).toBe(2);
+  });
+
+  it('should swap lines', () => {
+    fill();
+    container.swap(0, 1);
+    expect(container.lines[0].line).toBe('I used to be an adventurer, like you. Then I took an arrow in the knee');
+
+    container.swap(0, 2);
+    expect(container.lines[0].line).toBe(`You'll never take me alive, you robotic sumbitch!`);
+  });
+
+  it('should not swap when one of the indices is not in the lines array', () => {
+    const bestFableTitle = 'Chicken chaser! Do you chase chickens?';
+    fill();
+
+    container.swap(0, 42);
+    expect(container.lines[0].line).toBe(bestFableTitle);
+
+    container.swap(42, 0);
+    expect(container.lines[0].line).toBe(bestFableTitle);
+
+    container.swap(0, -1);
+    expect(container.lines[0].line).toBe(bestFableTitle);
+
+    container.swap(-1, 0);
+    expect(container.lines[0].line).toBe(bestFableTitle);
+  });
+
+  it('should clear', () => {
+    fill();
+    expect(container.empty).toBe(false);
+    container.clear();
+    expect(container.empty).toBe(true);
+  });
 });
--- src/app/model/dialogue/dialogue-line.ts
+++ src/app/model/dialogue/dialogue-line.ts
@@ -1,10 +1,54 @@
 import { ConditionDependent } from '../condition/condition-dependent';
 
+import { GameObject } from '../game-object';
+
 export class DialogueLine extends ConditionDependent {
   constructor(
     public line: string,
-    public isGreeting = false
+    public isGreeting: boolean = false
   ) {
     super();
+  }
+}
+
+export class LineContainer extends GameObject {
+  lines: DialogueLine[] = [];
+
+  [Symbol.iterator](): DialogueLine[] {
+    return this.lines;
+  }
+
+  get available(): DialogueLine[] {
+    return this.lines.filter((line: DialogueLine) => line.available);
+  }
+
+  get length(): number {
+    return this.lines.length;
+  }
+
+  get empty(): boolean {
+    return this.length === 0;
+  }
+
+  add(...lines: string[]): DialogueLine {
+    this.lines.push(...lines.map((line: string) => new DialogueLine(line)));
+
+    return this.lastOf(this.lines);
+  }
+
+  remove(index: number): DialogueLine {
+    return this.lines.splice(index, 1)[0];
+  }
+
+  swap(a: number, b: number): void {
+    if (a >= this.length || b >= this.length || a < 0 || b < 0) {
+      return;
+    }
+
+    this.lines[a] = this.lines.splice(b, 1, this.lines[a])[0];
+  }
+
+  clear(): void {
+    this.lines = [];
   }
 }
