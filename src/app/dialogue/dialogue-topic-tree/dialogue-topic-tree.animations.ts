36140dfb2344477d4d22a9ce2b9719d1a9a1ddf8
kuziemski.a@gmail.com <kuziemski.a@gmail.com>
2020-02-23 20:24:02 +0100

    feat: header animation

--- src/app/dialogue/dialogue-topic-tree/dialogue-topic-tree.animations.ts
+++ src/app/dialogue/dialogue-topic-tree/dialogue-topic-tree.animations.ts
@@ -7,7 +7,7 @@ const initialTopicState = { transform: 'scale(0.5)', opacity: 0 };
 const visibleTopicState = { transform: 'scale(1)', opacity: 1 };
 
 export const dialogueTreePanelAnimations: AnimationTriggerMetadata[] = [
-  trigger('topicItem', [
+  trigger('popInOut', [
     transition(':enter', [
       style(initialTopicState),
       animate('0.2s cubic-bezier(.8, -0.6, 0.2, 1.5)', style(visibleTopicState))
@@ -21,12 +21,24 @@ export const dialogueTreePanelAnimations: AnimationTriggerMetadata[] = [
     transition(':enter', [
       style(closedState),
       animate('0.2s ease-out', style(openedState)),
-      query('@topicItem', animateChild(), { optional: true }),
+      query('@popInOut', animateChild(), { optional: true }),
     ]),
     transition(':leave', [
       style(openedState),
-      query('@topicItem', animateChild(), { optional: true }),
+      query('@popInOut', animateChild(), { optional: true }),
       animate('0.2s ease-in', style(closedState)),
     ]),
+  ]),
+  trigger('moveOver', [
+    transition(':enter', [
+      style({ transform: 'translateX(-24px)' }),
+      animate('0.1s ease-in', style({ transform: 'translateX(0%)' }))
+    ])
+  ]),
+  trigger('squeezeOut', [
+    transition(':enter', [
+      style({ transform: 'translateX(24px)' }),
+      animate('0.1s ease-in', style({ transform: 'translateX(0%)' }))
+    ])
   ])
 ];
